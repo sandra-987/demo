81087e67a762a238c2cae85478d4dfecc18c1ea5
sandra 987 <sandrajohn898750@gmail.com>
2024-04-14 16:39:55 +0530

    add about

--- hello/home/views.py
+++ hello/home/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("this is homepage")
+def about(request):
+    return HttpResponse("this is about page")
