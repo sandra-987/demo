2847d3d2f42c2bf34c56d95e3d003a17303812c2
sandra 987 <sandrajohn898750@gmail.com>
2024-04-15 16:55:28 +0530

    add context

--- hello/home/views.py
+++ hello/home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    context={'variable':"this is sent"}
+    context={'variable':"obama is great"}
     return render(request,'index.html',context)
     #return HttpResponse("this is homepage")
 def about(request):
