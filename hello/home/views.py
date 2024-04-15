39039136d0017af3577318e202ebbb934d65aa18
sandra 987 <sandrajohn898750@gmail.com>
2024-04-15 16:52:17 +0530

    add context

--- hello/home/views.py
+++ hello/home/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("this is homepage")
+    context={'variable':"this is sent"}
+    return render(request,'index.html',context)
+    #return HttpResponse("this is homepage")
 def about(request):
     return HttpResponse("this is about page")
 def services(request):
